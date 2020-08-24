6fbd4ff99b54455adcb6d36b743ec475bb452b07
Olb2 <olgaborysovska@gmail.com>
2020-08-23 20:52:57 -0700

    fixed bug with auth form displaying incorrect content

--- src/Home-Login/Login.js
+++ src/Home-Login/Login.js
@@ -2,24 +2,22 @@ import React, { Component, useState, Fragment } from 'react'
 import './auth.css';
 import { withRouter}  from 'react-router-dom'; 
 
- class Login extends Component {
+ class Register extends Component {
     
 
     render() {
         return (
-        <div>
-            <div className='login'>
-            <form className="login-form">
-            <input type="text" placeholder="name"/>
-            <input type="password" placeholder="password"/>
-            <input type="text" placeholder="email address"/>
-            <button>create</button>
-            <p className="message">Already registered? <a href="#">Sign In</a></p>
-           </form>
-           </div> 
-           </div>
+            <div className='register'>
+            <form className="register-form">
+              <input type="text" placeholder="username"/>
+              <input type="password" placeholder="password"/>
+              <button>login</button>
+              <p >Not registered? <a href="#">Create an account</a></p>
+            </form>
+               </div>
+
         )
     }
 }
 
-export default Login
+export default Register
--- src/Home-Login/Register.js
+++ src/Home-Login/Register.js
@@ -2,20 +2,25 @@ import React, { Component, useState } from 'react'
 import './auth.css';
 import { withRouter}  from 'react-router-dom'; 
 
- class Register extends Component {
+ class Login extends Component {
 
     render() {
         return (
-        <div className='register'>
-    <form className="register-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <button>login</button>
-      <p >Not registered? <a href="#">Create an account</a></p>
-    </form>
+        <div>
+        <div className='login'>
+        <form className="login-form">
+        <input type="text" placeholder="name"/>
+        <input type="password" placeholder="password"/>
+        <input type="text" placeholder="email address"/>
+        <button>create</button>
+        <p className="message">Already registered? <a href="#">Sign In</a></p>
+       </form>
+       </div> 
        </div>
+
+
         )
     }
 }
 
-export default Register
+export default Login
