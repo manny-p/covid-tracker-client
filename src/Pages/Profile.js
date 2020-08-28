888ac4436d78422f6580e4a0b49140628c6109b2
joejons55 <vladomel55@gmail.com>
2020-08-28 02:32:52 -0700

    Created Grid.js for Profile Page, start woriking with aligning everything on a page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,40 +1,55 @@
 import React, { Component } from 'react'
-import TableHeading from "../components/TableHeading/TableHeading"
-import TableBody from "../components/TableBody/TableBody"
-import UserSideBar from '../components/UserSideBar/UserSideBar'
+import Container from '@material-ui/core/Container';
+import UserSpacingGrid from '../components/UserSideBar/ProfileGrid';
+// import TableHeading from "../components/TableHeading/TableHeading"
+// import TableBody from "../components/TableBody/TableBody"
+// import UserSideBar from '../components/UserSideBar/UserSideBar'
 
 
 class Profile extends Component {
     render() {
+        // const styles = {
+        //     backgroundColor: "black", 
+        //     position: "relative", 
+        //     minHeight: "100vh",
+        //     display: "flex",
+        //     flexDirection: "column",
+        //     justifyContent: "space-between",
+        //     color: "white"
+        // }
+        // const tableStyle = {
+        //     height: "2rem",
+        //     display: "flex",
+        //     flexDirection: "column",
+        //     alignItems: "center"
+        // }
         const styles = {
-            backgroundColor: "black", 
+            backgroundColor: "#3A4C8C", 
             position: "relative", 
-            minHeight: "100vh",
             display: "flex",
             flexDirection: "column",
-            justifyContent: "space-between",
-            color: "white"
-        }
-        const tableStyle = {
-            height: "2rem",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center"
+            alignItems: "center",
+            justifyContent: "space-between",    
         }
         
 
     return (
+        // <div style={styles}>
+        //     <h1 style={{textAlign: "center"}}>Your Saved Data</h1>
+        //     <div style={tableStyle}>
+        //         <TableHeading />
+        //         <TableBody />
+        //         <TableBody />
+        //         <TableBody />
+        //         <TableBody />
+        //         <TableBody />
+        //     </div>
+        //     <UserSideBar />
+        // </div>
         <div style={styles}>
-            <h1 style={{textAlign: "center"}}>Your Saved Data</h1>
-            <div style={tableStyle}>
-                <TableHeading />
-                <TableBody />
-                <TableBody />
-                <TableBody />
-                <TableBody />
-                <TableBody />
-            </div>
-            <UserSideBar />
+        <Container width="100%" fixed disableGutters={true}> 
+           <UserSpacingGrid /> 
+           </Container>
         </div>
     )
 }
