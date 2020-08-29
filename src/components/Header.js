09df157a2b5f1f9800b1909c5fd37a3b47fccac3
Olb2 <olgaborysovska@gmail.com>
2020-08-28 20:47:15 -0700

    added href tag to header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,11 @@ import { viruses } from '@fortawesome/free-solid-svg-icons'
 
  function Header() {
         
-        //  const element = <FontAwesomeIcon icon={<i class="fal fa-viruses"></i>} />
-
         return (
             <div className="flex-container">
-            <h1 className='logo'>
-          
-            <i class="fad fa-viruses fa-2x"></i>
-            </h1>
+            <a href="/" className='logo'>
+            <i class="fad fa-viruses fa-4x"></i>
+            </a>
               <ul className="navigation">
               <li><Link to={'/login'}>Login</Link></li>
               <li><Link to={'/register'}>Sign Up</Link></li>
